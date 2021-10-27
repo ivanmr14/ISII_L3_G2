1d4b7e9bf890d29ca04efeba211f9f69e5140720
alumno <alumno@147.156.80.203>
2021-10-27 12:03:32 +0200

    Importo controlador en clase registrarentrada

--- Camping_Party/src/main/java/campingparty/RegistrarEntrada.java
+++ Camping_Party/src/main/java/campingparty/RegistrarEntrada.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package campingparty;
+import Modelo.Controlador;
 import java.util.ArrayList;
 import javax.swing.DefaultListModel;
 
