1e5f7e07e92c14f47ae0425ac1c4d4efc0509d0c
MIRENA <MIRENA@DESKTOP-I70BCEK>
2021-10-27 11:59:35 +0200

    quitada la clase controlador de campingparty

--- Camping_Party/src/main/java/Modelo/RegistroCliente.java
+++ Camping_Party/src/main/java/Modelo/RegistroCliente.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package campingparty;
+package Modelo;
+import campingparty.Controlador;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import javax.swing.JFrame;
